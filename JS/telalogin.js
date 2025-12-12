660a29a7d2294571ced435825397bec04c46d2ba
WsDuque <wendells.duque@gmail.com>
2025-12-12 10:38:30 -0300

    telogin

--- JS/telalogin.js
+++ JS/telalogin.js
@@ -1,12 +1,10 @@
-import { carregarDetalhesLivro, renderizarDetalhesLivro } from "./detalhes";
-
 const API_URL = "http://localhost:3000";
 var formLogin = document.getElementById('formLogin');
 var chk = document.getElementById('btnMostrarSenha');
 var senha = document.getElementById('password');
 var username = document.getElementById('username');
 var lblEsqueciSenha = document.getElementById('lblEsqueciSenha');
-var id = null;
+export var id = null;
 async function extrairListaUsuarios() {
     try {
         const resp = await fetch(`${API_URL}/usuarios`);
@@ -28,24 +26,23 @@ document.addEventListener('DOMContentLoaded', function () {
     formLogin.addEventListener('submit', async function (e) {
         e.preventDefault();
         const listaUsuarios = await extrairListaUsuarios();
-        if (username && senha && listaUsuarios.length > 0) {
-            const usuarioEncontrado = listaUsuarios.some(perfil =>
-                perfil.nome === username.value || perfil.email === username.value
-            );
-            const senhaCorreta = listaUsuarios.some(perfil =>
-                perfil.senha === senha.value
-            );
-            if (usuarioEncontrado && senhaCorreta) {
-                const id = listaUsuarios.find(perfil =>
-                    (perfil.nome === username.value || perfil.email === username.value) && perfil.senha === senha.value
-                ).id;
-                window.location.href = '/FrontEnd/telaInicial.html';
-                alert('ID do usuário:', id);
-            } else {
-                alert('Usuário ou senha inválidos!');
+        
+        if (!username || !senha) return;
+
+        const usernameValue = username.value;
+        const senhaValue = senha.value;
+        const usuarioEncontrado = listaUsuarios.find(perfil =>
+            (perfil.nome === usernameValue || perfil.email === usernameValue) && perfil.senha === senhaValue
+        );
+
+        if (usuarioEncontrado) {
+            console.log('Usuário encontrado:', usuarioEncontrado);
+            id = usuarioEncontrado.id;
+            localStorage.setItem('userId', String(id));
+            console.log('ID salvo no localStorage:',id);
+            // window.location.href = '/FrontEnd/telainicial.html';
             }
-        } else {
-            alert('Por favor, preencha os campos obrigatórios.');
-        }
-    });
+             
+        } 
+    );
 });
